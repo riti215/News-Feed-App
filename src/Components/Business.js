bff1941143d68820242776d9817596abb34df55b
Ritika Parashar <57587827+riti215@users.noreply.github.com>
2020-10-11 21:59:52 +0530

    Update Business.js

--- src/Components/Business.js
+++ src/Components/Business.js
@@ -10,7 +10,7 @@ class Business extends Component {
 
   componentDidMount() {
     const app_key = "273850e5347b4af0ae7c082f104ea3f8";
-    const business_url = `http://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=${app_key}`;
+    const business_url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=${app_key}`;
     fetch(business_url)
       .then((response) => response.json())
       .then((myJson) => {
